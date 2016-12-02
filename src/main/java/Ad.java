f328d27fae0fb213138d38a52e8d5c30640f45ba
Zach Gulde <zachgulde@gmail.com>
2016-12-02 10:48:59 -0600

    add Ads interface and DAO implementation

---
+++ src/main/java/Ad.java
@@ -0,0 +1,45 @@
+public class Ad {
+    private long id;
+    private long userId;
+    private String title;
+    private String description;
+
+    public Ad(long id, long userId, String title, String description) {
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/ListAdsDao.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAdsDao implements Ads {
+    private List<Ad> ads;
+
+    public List<Ad> all() {
+        if (ads == null) {
+            ads = generateAds();
+        }
+        return ads;
+    }
+
+    private List<Ad> generateAds() {
+        List<Ad> ads = new ArrayList<>();
+        ads.add(new Ad(
+            1,
+            1,
+            "playstation for sale",
+            "This is a slightly used playstation"
+        ));
+        ads.add(new Ad(
+            2,
+            1,
+            "Super Nintendo",
+            "Get your game on with this old-school classic!"
+        ));
+        ads.add(new Ad(
+            3,
+            2,
+            "Junior Java Developer Position",
+            "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
+        ));
+        ads.add(new Ad(
+            4,
+            2,
+            "JavaScript Developer needed",
+            "Must have strong Java skills"
+        ));
+        return ads;
+    }
+}
