14910218c788eb93f5df5aa169bca64a10e0810d
Zach Gulde <zachgulde@gmail.com>
2016-12-01 18:06:13 -0600

    initial commit
    - pom.xml setup to include servlets
    - skeleton of a hello world servlet

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,9 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    }
+}
