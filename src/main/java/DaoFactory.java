3604f194fe74a28d7468a186490d5e64884766af
Zach Gulde <zachgulde@gmail.com>
2016-12-02 10:48:59 -0600

    add Ads interface and DAO implementation

---
+++ src/main/java/DaoFactory.java
@@ -0,0 +1,10 @@
+public class DaoFactory {
+    private static Ads adsDao;
+
+    public static Ads getAdsDao() {
+        if (adsDao == null) {
+            adsDao = new ListAdsDao();
+        }
+        return adsDao;
+    }
+}
