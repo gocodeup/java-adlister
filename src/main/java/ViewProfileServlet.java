f911119de36ce023eecb46be98129a1282495bcc
Zach Gulde <zachgulde@gmail.com>
2016-12-06 11:18:51 -0600

    show profile page from servlet
    instead of a jsp

---
+++ src/main/java/ViewProfileServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
+public class ViewProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+    }
+}
