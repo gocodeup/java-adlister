b4e94857453e1e5a948aac8c76854efb3e3b3a0a
Zach Gulde <zachgulde@gmail.com>
2016-12-08 15:26:35 -0600

    move configuration to external class
    our sensitive information will live in Config.java, and we will not
    track it with git

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -2,10 +2,11 @@ package com.codeup.adlister.dao;
 
 public class DaoFactory {
     private static Ads adsDao;
+    private static Config config = new Config();
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
-            adsDao = new MySQLAdsDao();
+            adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
     }
--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -3,6 +3,9 @@ package com.codeup.adlister.dao;
 import com.codeup.adlister.models.Ad;
 import com.mysql.cj.jdbc.Driver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,16 +13,16 @@ import java.util.List;
 public class MySQLAdsDao implements Ads {
     private Connection connection = null;
 
-    public MySQLAdsDao() {
+    public MySQLAdsDao(Config config) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost/adlister_db",
-                "root",
-                "codeup"
+                config.getUrl(),
+                config.getUser(),
+                config.getPassword()
             );
         } catch (SQLException e) {
-            throw new RuntimeException("Error talking to the database!", e);
+            throw new RuntimeException("Error connecting to the database!", e);
         }
     }
 
