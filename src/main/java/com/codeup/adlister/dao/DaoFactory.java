66260b86eeda768d3bb0e8742002ae245db4b69f
Zach Gulde <zachgulde@gmail.com>
2016-12-09 11:51:46 -0600

    implement mysql users dao

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.dao;
 
 public class DaoFactory {
     private static Ads adsDao;
+    private static Users usersDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -9,5 +10,12 @@ public class DaoFactory {
             adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
+    }
+
+    public static Users getUsersDao() {
+        if (usersDao == null) {
+            usersDao = new MySQLUsersDao(config);
+        }
+        return usersDao;
     }
 }
