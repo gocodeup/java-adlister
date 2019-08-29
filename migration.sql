cebe8b30ad551ab6ed01409943bc0a4357490da9
JustinSOrtega <justinsortega@gmail.com>
2019-08-29 11:20:00 -0500

    added new mysql table categories

--- migration.sql
+++ migration.sql
@@ -2,21 +2,33 @@ USE adlister_db;
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
 
-CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE users
+(
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
+    email    VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
+CREATE TABLE ads
+(
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id     INT UNSIGNED NOT NULL,
+    title       VARCHAR(240) NOT NULL,
+    description TEXT         NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
         ON DELETE CASCADE
 );
+
+CREATE TABLE categories
+(
+    ads_id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    categories     varchar(240)  NOT NULL,
+    FOREIGN KEY (ads_id) REFERENCES ads (id)
+        ON DELETE CASCADE
+);
+
