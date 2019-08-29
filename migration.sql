7c30c25fc6a15c869c8c2e25f0a919f836ef84f3
Steven Turner <steven.turner12x@gmail.com>
2019-08-29 11:52:57 -0500

    formatting

--- migration.sql
+++ migration.sql
@@ -5,41 +5,37 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS blocks;
 
-CREATE TABLE users
-(
-    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE users(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(240) NOT NULL,
-    email    VARCHAR(240) NOT NULL,
+    email VARCHAR(240) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+        PRIMARY KEY (id)
 );
 
-CREATE TABLE ads
-(
-    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id     INT UNSIGNED NOT NULL,
-    title       VARCHAR(240) NOT NULL,
-    description TEXT         NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
-        ON DELETE CASCADE
+CREATE TABLE ads(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(240) NOT NULL,
+    description TEXT NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (user_id) REFERENCES users (id)
+            ON DELETE CASCADE
 );
 
-CREATE TABLE categories
-(   
-    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    category     varchar(240)  NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE categories(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category varchar(240)  NOT NULL,
+        PRIMARY KEY (id)
 );
 
 CREATE TABLE blocks (
-                    id INT UNSIGNED NOT NULL  AUTO_INCREMENT,
-                    block VARCHAR(240) NOT NULL
-                    PRIMARY KEY (id) 
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    block VARCHAR(240) NOT NULL,
+        PRIMARY KEY (id)
 );
 
-CREATE TABLE adsCategoriesBlocks
-(
+CREATE TABLE adsCategoriesBlocks(
     ads_id INT UNSIGNED NOT NULL,
     blocks_id INT UNSIGNED NOT NULL,
     categories_id INT UNSIGNED NOT NULL,
