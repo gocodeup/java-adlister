3fc191335204617c0a52ede7c8b5658c77dc6eb1
Brandi <brandiclinard021911@gmail.com>
2019-08-29 11:01:03 -0500

    category and block tables added to migration

---
+++ categories-blocks-seeder.sql
@@ -0,0 +1,4 @@
+USE adlister_db;
+
+INSERT INTO categories (category) /*can insert here in any order but must input the values according to this setup*/
+VALUES   ('Michael Jackson', 'Thriller', '1982', '47.3', 'Pop, Rock, R&B'),
