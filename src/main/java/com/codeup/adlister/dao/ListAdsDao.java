2387abece44a73109d1635bde1ec18c0cd11e08f
JustinSOrtega <justinsortega@gmail.com>
2019-08-30 14:46:44 -0500

    pulling fixed

--- src/main/java/com/codeup/adlister/dao/ListAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/ListAdsDao.java
@@ -1,76 +1,62 @@
-package com.codeup.adlister.dao;
-
-import com.codeup.adlister.models.Ad;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ListAdsDao implements Ads {
-    private List<Ad> ads;
-
-    public List<Ad> all() {
-        if (ads == null) {
-            ads = generateAds();
-        }
-        return ads;
-    }
-
-    public Long insert(Ad ad) {
-        // make sure we have ads
-        if (ads == null) {
-            ads = generateAds();
-        }
-        // we'll assign an "id" here based on the size of the ads list
-        // really the dao would handle this
-<<<<<<< HEAD
-        ad.setId(ads.size());
-=======
-        ad.setId((long) ads.size());
->>>>>>> e076e545f3f9fe01d71185c5a383495f8ede8a36
-        ads.add(ad);
-        return ad.getId();
-    }
-
-<<<<<<< HEAD
-    @Override
-    public List<Ad> userAds(Long user_id) {
-        return null;
-    }
-
-    private List<Ad> generateAds() {
-        List<Ad> ads = new ArrayList<>();
-        ads.add(new Ad(
-            1,
-            1,
-            "playstation for sale",
-            "This is a slightly used playstation"
-        ));
-        ads.add(new Ad(
-            2,
-            1,
-            "Super Nintendo",
-            "Get your game on with this old-school classic!"
-        ));
-        ads.add(new Ad(
-            3,
-            2,
-            "Junior Java Developer Position",
-            "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
-        ));
-        ads.add(new Ad(
-            4,
-            2,
-            "JavaScript Developer needed",
-            "Must have strong Java skills"
-        ));
-        return ads;
-    }
-}
-=======
-    private List<Ad> generateAds() {
-        List<Ad> ads = new ArrayList<>();
-        return ads;
-    }
-}
-
->>>>>>> e076e545f3f9fe01d71185c5a383495f8ede8a36
+//package com.codeup.adlister.dao;
+//
+//import com.codeup.adlister.models.Ad;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class ListAdsDao implements Ads {
+//    private List<Ad> ads;
+//
+//    public List<Ad> all() {
+//        if (ads == null) {
+//            ads = generateAds();
+//        }
+//        return ads;
+//    }
+//
+//    public Long insert(Ad ad) {
+//        // make sure we have ads
+//        if (ads == null) {
+//            ads = generateAds();
+//        }
+//        // we'll assign an "id" here based on the size of the ads list
+//        // really the dao would handle this
+//        ad.setId(ads.size());
+//        ads.add(ad);
+//        return ad.getId();
+//    }
+//
+//    @Override
+//    public List<Ad> userAds(Long user_id) {
+//        return null;
+//    }
+//    private List<Ad> generateAds() {
+//        List<Ad> ads = new ArrayList<>();
+//        ads.add(new Ad(
+//            1,
+//            1,
+//            "playstation for sale",
+//            "This is a slightly used playstation"
+//        ));
+//        ads.add(new Ad(
+//            2,
+//            1,
+//            "Super Nintendo",
+//            "Get your game on with this old-school classic!"
+//        ));
+//        ads.add(new Ad(
+//            3,
+//            2,
+//            "Junior Java Developer Position",
+//            "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
+//        ));
+//        ads.add(new Ad(
+//            4,
+//            2,
+//            "JavaScript Developer needed",
+//            "Must have strong Java skills"
+//        ));
+//        return ads;
+//    }
+//}
--- src/main/java/com/codeup/adlister/models/Ad.java
+++ src/main/java/com/codeup/adlister/models/Ad.java
@@ -1,7 +1,5 @@
 package com.codeup.adlister.models;
-
 import java.util.Date;
-
 public class Ad {
     private long id;
     private long userId;
@@ -10,85 +8,63 @@ public class Ad {
     private Date date;
     private int blocksId;
     private int categoriesId;
-
     public Ad() {
-
-
     }
-
-
     public Ad(long id, long userId, String title, String description, Date date, int blocksId, int categoriesId) {
         this.id = id;
         this.userId = userId;
         this.title = title;
         this.description = description;
         this.date = date;
-        this. blocksId = blocksId;
-        this. categoriesId = categoriesId;
+        this.blocksId = blocksId;
+        this.categoriesId = categoriesId;
     }
-
-
     public Ad(long userId, String title, String description, int blocksId, int categoriesId) {
         this.userId = userId;
         this.title = title;
         this.description = description;
         this.blocksId = blocksId;
-        this.categoriesId= categoriesId;
-
+        this.categoriesId = categoriesId;
     }
-
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public long getUserId() {
         return userId;
     }
-
     public void setUserId(long userId) {
         this.userId = userId;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
-    public java.sql.Date getDate() {
-        return (java.sql.Date) date;
+    public Date getDate() {
+        return date;
     }
-
     public void setDate(Date date) {
         this.date = date;
     }
-
     public int getBlocksId() {
         return blocksId;
     }
-
     public void setBlocksId(int blocksId) {
         this.blocksId = blocksId;
     }
-
     public int getCategoriesId() {
         return categoriesId;
     }
-
     public void setCategoriesId(int categoriesId) {
         this.categoriesId = categoriesId;
     }
