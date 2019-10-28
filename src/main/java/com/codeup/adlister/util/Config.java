7e1aeb3cb8076d7f7ef7d73914b01f5b3925584d
yaelBrown <yaelrbrown@gmail.com>
2019-10-28 16:15:12 -0500

    Commented out config in gitignore

---
+++ src/main/java/com/codeup/adlister/util/Config.java
@@ -0,0 +1,14 @@
+class Config {
+    public String getUrl() {
+        return "jdbc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false";
+    }
+    public String getUsername() {
+        return "root";
+    }
+    public String getPassword() {
+        return "codeup";
+    }
+}
+
+
+
