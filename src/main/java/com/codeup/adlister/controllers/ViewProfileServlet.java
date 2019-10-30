4733d92dfaead9d8ccad8c2b1e6793f67e98f6d2
Jason Kempel <kempel.jason13@gmail.com>
2019-10-30 13:09:19 -0500

    corrected conflicts

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,16 +13,13 @@ import java.io.IOException;
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-<<<<<<< HEAD
         String user = request.getSession().getAttribute("user").toString();
-=======
->>>>>>> 918c5ae8069317d9e258b0c919cba7ac4344cbb1
+
         if (request.getSession().getAttribute("user") == null) {
             response.sendRedirect("/login");
 
             return;
         }
-<<<<<<< HEAD
         User uID = (User) request.getSession().getAttribute("user");
 //            I want what ever ad this user created to display on the user's profile.
 //            need to take in any info from the "/ads" and somehow transfer to user's profile
@@ -31,8 +31,7 @@ public class ViewProfileServlet extends HttpServlet {
     }
 
 }
-=======
-        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
-    }
-}
->>>>>>> 918c5ae8069317d9e258b0c919cba7ac4344cbb1
+
+
+
+
