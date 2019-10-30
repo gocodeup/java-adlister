f8a6ca77e8fad5976fefb5bc72a0fcd276c670d8
Gloria Laura Vela <gloria.laura.vela@gmail.com>
2019-10-30 13:54:00 -0500

    unique user

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -24,6 +24,7 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public User findByUsername(String username) {
+
         String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
         try {
             PreparedStatement stmt = connection.prepareStatement(query);
@@ -36,18 +37,25 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public Long insert(User user) {
-        String query = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
-        try {
-            PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, user.getUsername());
-            stmt.setString(2, user.getEmail());
-            stmt.setString(3, user.getPassword());
-            stmt.executeUpdate();
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            return rs.getLong(1);
-        } catch (SQLException e) {
-            throw new RuntimeException("Error creating new user", e);
+        MySQLUsersDao adConnection = new MySQLUsersDao(new Config());
+        User duplicateUser = adConnection.findByUsername(user.getUsername());
+        if (duplicateUser != null) {
+            return -1L;
+
+        } else {
+            String query = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+            try {
+                PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+                stmt.setString(1, user.getUsername());
+                stmt.setString(2, user.getEmail());
+                stmt.setString(3, user.getPassword());
+                stmt.executeUpdate();
+                ResultSet rs = stmt.getGeneratedKeys();
+                rs.next();
+                return rs.getLong(1);
+            } catch (SQLException e) {
+                throw new RuntimeException("Error creating new user", e);
+            }
         }
     }
 
@@ -71,9 +79,9 @@ public class MySQLUsersDao implements Users {
             stmt.setLong(1, adId);
             stmt.executeQuery();
             ResultSet rs = stmt.getResultSet();
-            System.out.println("executed query");
-            System.out.println("ad id = " + adId);
-            System.out.println(rs);
+//            System.out.println("executed query");
+//            System.out.println("ad id = " + adId);
+//            System.out.println(rs);
 //            while (rs.next())
 //                System.out.println(rs.getString("username"));
 //            User u = new User(
