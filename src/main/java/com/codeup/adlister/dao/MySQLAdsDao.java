e5ca80e7ac80ece319babd1e181aa8856bb8e38b
Jason Kempel <kempel.jason13@gmail.com>
2019-10-30 12:33:29 -0500

    fixed viewing individual ads

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -76,6 +76,7 @@ public class MySQLAdsDao implements Ads {
 
     private Ad extractAd(ResultSet rs) throws SQLException {
         return new Ad(
+            rs.getLong("id"),
             rs.getLong("user_id"),
             rs.getString("title"),
             rs.getString("description"),
