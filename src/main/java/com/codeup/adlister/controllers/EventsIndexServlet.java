6b942379f17b32fd9c6788d1176376c3e0f2dba7
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-06-16 12:16:08 -0500

    feat: events page viewable

--- src/main/java/com/codeup/adlister/controllers/EventsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EventsIndexServlet.java
@@ -1,5 +1,4 @@
 package com.codeup.adlister.controllers;
-
 import com.codeup.adlister.dao.DaoFactory;
 
 import javax.servlet.ServletException;
@@ -9,11 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ads")
-public class AdsIndexServlet extends HttpServlet {
+@WebServlet(name = "controllers.EventsIndexServlet", urlPatterns = "/events")
+public class EventsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("ads", DaoFactory.getAdsDao().all());
-
-        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+//        if (request.getSession().getAttribute("events").equals("events")){
+//            response.sendRedirect("/events");
+//        }
+        request.getRequestDispatcher("/WEB-INF/events/index.jsp").forward(request, response);
     }
+
 }
--- src/main/webapp/WEB-INF/events/index.jsp
+++ src/main/webapp/WEB-INF/events/index.jsp
@@ -1,16 +1,23 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: nayoungkim
+  Date: 6/16/20
+  Time: 12:01 PM
+  To change this template use File | Settings | File Templates.
+--%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <jsp:include page="/WEB-INF/partials/head.jsp">
-        <jsp:param name="title" value="Viewing All The Ads" />
+        <jsp:param name="title" value="Events" />
     </jsp:include>
 </head>
 <body>
 <jsp:include page="/WEB-INF/partials/navbar.jsp" />
 
 <div class="container mt-4">
-    <h1>Here Are all the ads!</h1>
+
+    <h1>Here are your events</h1>
 
     <c:forEach var="ad" items="${ads}">
         <div class="col-md-6">
