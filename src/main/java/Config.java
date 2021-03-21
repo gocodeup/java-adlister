34b2177e1bd6f98a9aef61a9fe3e4a906f5ec71b
kuwan barnes <Kuwan.barne@gmail.com>
2021-03-21 13:50:54 -0500

    commits

---
+++ src/main/java/Config.java
@@ -0,0 +1,11 @@
+class Config {
+    public String getUrl() {
+        return "jdbc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false";
+    }
+    public String getUser() {
+        return "root";
+    }
+    public String getPassword() {
+        return "codeup";
+    }
+}
