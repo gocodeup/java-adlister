20aa31ced931c3973e8b7ba2a814a7e7692ca3e2
Hunter Parks <parks.hunter1992@gmail.com>
2021-05-26 11:28:15 -0500

    working on first bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,20 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+@WebServlet(name = "HelloWorld", urlPatterns = "/hello")// metadata to go along with our servlet
+public class HelloWorldServlet extends HttpServlet { // defining the class name / extending from HttpServlet
+   // Put doGet doPost methods in here
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String name = request.getParameter("name");
+
+        response.getWriter().println("<h1>Hello World!</h1");
+
+
 
     }
 }
