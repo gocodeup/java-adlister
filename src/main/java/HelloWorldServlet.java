15c7f44790b9190ce3f5382c258601f2743e363a
Hunter Parks <parks.hunter1992@gmail.com>
2021-05-26 12:42:30 -0500

    finshed the count exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -8,25 +8,13 @@ import java.io.IOException;
 
     @WebServlet(name = "HelloWorld", urlPatterns = "/hello")// metadata to go along with our servlet
     public class HelloWorldServlet extends HttpServlet { // defining the class name / extending from HttpServlet
-//        private int counter = 0;
-//
-//        public void init() {
-//
-//        }
+
         // Put doGet doPost methods in here
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
             String name = request.getParameter("name");
             response.getWriter().println("<h1>Hello World!</h1");
             response.getWriter().printf("<h1>Hello, %s !</h1>", name);
-
-//            counter++;
-//            response.getWriter().println("<h3>This is how many times you viewed the page: " + counter + " </h3>");
-
-
-
-
-
 
 
         }
