26f7be87ab08540fa10f0e41154f5f5070e8757a
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-05-28 15:08:51 -0500

    feat:Guessing game

---
+++ src/main/java/GuessServlet.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name="GuessServlet", urlPatterns = "/guess")
+
+public class GuessServlet extends HttpServlet {
+@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String guess = request.getParameter("guess");
+        int random = (int) Math.floor((Math.random() * 3) + 1);
+        String randomNumString = Integer.toString(random);
+
+         if(guess.equalsIgnoreCase(randomNumString)){
+            response.sendRedirect("/correct.jsp");
+        }else if(!guess.equalsIgnoreCase(randomNumString)){
+            response.sendRedirect("/incorrect.jsp");
+        }
+    }
+@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect("/guess.jsp");
+
+    }
+}
