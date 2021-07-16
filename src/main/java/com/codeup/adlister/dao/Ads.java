4c16af3e921834182d1ea96fae07eb7b80d9c81d
David Culver <culver.david.p@gmail.com>
2021-07-16 11:37:27 -0500

    completed search ads task

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -7,8 +7,13 @@ import java.util.List;
 public interface Ads {
     // get a list of all the ads
     List<Ad> all();
+
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
+
     //    return list of ads for individual user
     List<Ad> displayAdsFromUserId(Long userId);
+
+    //  return list of ads based on a user search
+    List<Ad> searchForAds(String searchForAd);
 }
--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -87,4 +87,23 @@ public class MySQLAdsDao implements Ads {
         }
         return usersAds;
     }
+
+    public List<Ad> searchForAds(String searchForAd) {
+        List<Ad> adsFromSearch = new ArrayList<>();
+        String sql = "SELECT * FROM ads WHERE title LIKE ? OR description LIKE ?;";
+        String searchUsingUserInput = "%" + searchForAd + "%";
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, searchUsingUserInput);
+            stmt.setString(2, searchUsingUserInput);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                adsFromSearch.add(extractAd(rs));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all ads.", e);
+        }
+        return adsFromSearch;
+    }
 }
