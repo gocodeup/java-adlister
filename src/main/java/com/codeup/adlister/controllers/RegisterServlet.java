4dedc574dcba31853a231bf0f89055cbec4b3dca
Alex Santamaria <santamariaalex146@gmail.com>
2021-07-16 16:19:07 -0500

    tried working on error messages

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -9,14 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+//        request.getRequestDispatcher("/WEB-INF/foo.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        Map<String, String> messages = new HashMap<String, String>();
+//        request.setAttribute("messages", messages);
+
         String username = request.getParameter("username");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
@@ -33,9 +39,31 @@ public class RegisterServlet extends HttpServlet {
             return;
         }
 
+//        String foo = request.getParameter("foo");
+//        if (foo == null || foo.trim().isEmpty()) {
+//            messages.put("foo", "Please enter foo");
+//        } else if (!foo.matches("\\p{Alnum}+")) {
+//            messages.put("foo", "Please enter alphanumeric characters only");
+//        }
+//
+//        String bar = request.getParameter("bar");
+//        if (bar == null || bar.trim().isEmpty()) {
+//            messages.put("bar", "Please enter bar");
+//        } else if (!bar.matches("\\d+")) {
+//            messages.put("bar", "Please enter digits only");
+//        }
+//
+//        // ...
+//
+//        if (messages.isEmpty()) {
+//            messages.put("success", "Form successfully submitted!");
+//        }
+//
+//        request.getRequestDispatcher("/WEB-INF/foo.jsp").forward(request, response);
 
 
-        // create and save a new user
+
+//         create and save a new user
         User user = new User(username, email, password);
         DaoFactory.getUsersDao().insert(user);
         response.sendRedirect("/login");
