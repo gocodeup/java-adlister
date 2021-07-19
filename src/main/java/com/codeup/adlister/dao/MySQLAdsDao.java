bd1bd34a85eb3f8066002257fb3a86dc0a264115
Jordan Felan <felan.jordan94@gmail.com>
2021-07-18 23:11:06 -0500

    minor changes to master

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -40,8 +40,13 @@ public class MySQLAdsDao implements Ads {
     }
 
     @Override
-
     public long insertAd(Ad ad) {
+        return 0;
+    }
+
+    @Override
+
+
 
     public List<Ad> findAdsByUserId(long userId) {
         PreparedStatement stmt = null;
@@ -162,6 +167,11 @@ public class MySQLAdsDao implements Ads {
             throw new RuntimeException("Error updating Profile information", e);
         }
 
+    }
+
+    @Override
+    public List<Ad> all() {
+        return null;
     }
 //    @Override
 //    public long insertAdCategories(Category category) throws SQLException {
