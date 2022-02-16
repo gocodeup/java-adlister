4373d09fb2c869cbb75952b34c7cfedbf45ba293
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-02-16 17:30:06 -0600

    MIGRATION file changed

--- migration.sql
+++ migration.sql
@@ -1,5 +1,6 @@
 USE adlister_db;
 
+
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS categories;
@@ -11,35 +12,68 @@ CREATE TABLE users
     username VARCHAR(240) NOT NULL UNIQUE,
     email    VARCHAR(240) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+
+# added these ND
+dateCreated VARCHAR(50) NOT NULL,
+UNIQUE (username),
+UNIQUE (email)
+
 );
 
-CREATE TABLE ads
+CREATE TABLE IF NOT EXISTS ads
 (
     id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id     INT UNSIGNED NOT NULL,
     title       VARCHAR(240) NOT NULL,
-    description TEXT         NOT NULL,
+    description VARCHAR(400)    NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+
+# added these ND
+dateCreated VARCHAR (50) NOT NULL,
+categoryName VARCHAR(255) NOT NULL,
+UNIQUE (user_id, title, description)
 );
 
-CREATE TABLE categories
-(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    ad_id INT UNSIGNED NOT NULL,
-    categoryName VARCHAR(240) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (ad_id) REFERENCES ads (id)
-        ON DELETE CASCADE
+
+
+
+#     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     ad_id INT UNSIGNED NOT NULL,
+#     categoryName VARCHAR(240) NOT NULL,
+#     PRIMARY KEY (id),
+#     FOREIGN KEY (ad_id) REFERENCES ads (id)
+#         ON DELETE CASCADE,
+
+
+# added this ND
+# it has an error with the foreign key other code
+
+CREATE TABLE IF NOT EXISTS categories
+(id INT UNSIGNED AUTO_INCREMENT,
+categoryName VARCHAR(100) NOT NULL ,
+PRIMARY KEY (id),
+UNIQUE (categoryName)
 );
 
 CREATE TABLE ad_category
 (
-    category_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    ad_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    ad_id INT UNSIGNED NOT NULL ,
     FOREIGN KEY (ad_id) REFERENCES ads (id)
         ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
+
+
+INSERT INTO categories (categoryName) VALUES
+
+    ('for sale'),
+    ('automotive'),
+    ('jobs'),
+    ('community'),
+    ('furniture');
+
+SELECT * FROM categories;
