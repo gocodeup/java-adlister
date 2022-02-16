78ca6d3d17487495c3899c5367014932435a7a75
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-02-16 10:17:02 -0600

    added comments to better understand what needs to be done

--- src/main/java/com/codeup/adlister/controllers/ShowAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ShowAdServlet.java
@@ -10,8 +10,11 @@ import java.io.IOException;
 @WebServlet("/ad/id")
 public class ShowAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        Long id = Long.valueOf(req.getParameter("id"));
-        req.setAttribute("ad", id);
+        long id = Long.parseLong(req.getParameter("id"));
+//      need to get id of ad
+        //  here we will bring in DaoFactory here to getAdById()
+//      need to set id of ad to "ad"
+//      req.setAttribute("ad", id)
         req.getRequestDispatcher("/WEB-INF/ads/showAd.jsp").forward(req, res);
     }
 
