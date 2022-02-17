44904bb9f3dbe1145a4605b375366d303b84bb34
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-02-17 16:31:51 -0600

    commenting out code to rethink

--- src/main/java/com/codeup/adlister/controllers/ShowAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ShowAdServlet.java
@@ -1,5 +1,6 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.Ads;
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
 
@@ -9,15 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet("/ad")
 public class ShowAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-//        long id = Long.parseLong(req.getParameter("id"));
+        long id = Long.parseLong(req.getParameter("id"));
         String adId = req.getParameter("id");
         req.setAttribute("id", adId);
+//        try {
+//            Ad adsDao = DaoFactory.getAdsDao().findOne(id);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
 //        if(adId != null){
 //
 //        }
