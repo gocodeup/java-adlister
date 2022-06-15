600acb4f75cc6917beae5cc48d385aaa57ebf0fc
Boryana1 <boryana.dimova1@gmail.com>
2022-06-15 12:16:40 -0500

    updated with request.getDispatcher()...

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -6,7 +7,8 @@ import java.io.IOException;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        response.getWriter().println("<h1>Hello, World!</h1>");
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: boryanadimova
+  Date: 6/14/22
+  Time: 3:10 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Title</title>
+  </head>
+  <body>
+  
+  </body>
+</html>
