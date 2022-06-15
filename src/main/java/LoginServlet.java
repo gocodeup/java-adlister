52835f8cee0901da628cec70d5a695f35b1acd15
Boryana1 <boryana.dimova1@gmail.com>
2022-06-15 12:17:06 -0500

    done with login Servlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Login {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class  LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
 }
