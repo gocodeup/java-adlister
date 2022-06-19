a328ad8cf73bdd92ed8355184d428cb12943bc0c
Boryana1 <boryana.dimova1@gmail.com>
2022-06-19 11:10:15 -0500

    added a doPost method

--- src/main/java/PizzaOrder.java
+++ src/main/java/PizzaOrder.java
@@ -1,4 +1,5 @@
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,8 +8,22 @@ import java.io.IOException;
 
 @WebServlet(name = "PizzaOrder", urlPatterns = "/pizza-order")
 public class PizzaOrder extends HttpServlet{
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getRequestDispatcher("/pizza-order.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(request.getParameter("crust-type"));
+        System.out.println(request.getParameter("sauce-type"));
+        String[] toppings = request.getParameterValues("toppings");
+        for(String topping : toppings){
+            System.out.println(topping);
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+        System.out.println(request.getParameter("address"));
+        response.sendRedirect("/");
     }
 }
