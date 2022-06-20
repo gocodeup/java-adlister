9e40f4c56834679fe90c185200dd6b47c94badbd
Boryana1 <boryana.dimova1@gmail.com>
2022-06-20 16:35:51 -0500

    completed

--- src/main/java/PickColorServlet.java
+++ src/main/java/PickColorServlet.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class PickColorServlet {
-}
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/pick-color")
+public class PickColorServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/pick-color.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String color = request.getParameter("color");
+        response.sendRedirect("/view-color?viewcolor=" + color); //query string
+    }
+ }
