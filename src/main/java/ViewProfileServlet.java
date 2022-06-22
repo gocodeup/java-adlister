cc2c79df29ba3bdb03de81ec8070a54e7ca20f16
Boryana1 <boryana.dimova1@gmail.com>
2022-06-22 06:12:40 -0500

    added doGet

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ViewProfileServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
+public class ViewProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/profile.jps").forward(request
+        , response);
+    }
 }
