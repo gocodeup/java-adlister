2251a9fe4a10431c1e1806db37c07e404b5620e2
Boryana1 <boryana.dimova1@gmail.com>
2022-06-22 06:14:24 -0500

    created

--- src/main/java/CreateAdServlet.java
+++ src/main/java/CreateAdServlet.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class CreateAdServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
+public class CreateAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        Ad ad = new Ad(
+                user.getId(),
+                request.getParameter("title"),
+                request.getParameter("description")
+        );
+        DaoFactory.getAdsDao().insert(ad);
+        response.sendRedirect("/ads");
+    }
 }
