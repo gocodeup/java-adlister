e0faa9a0b4671e78829a826858032738e3f163e8
Boryana1 <boryana.dimova1@gmail.com>
2022-06-22 06:14:38 -0500

    created

--- src/main/java/AdsIndexServlet.java
+++ src/main/java/AdsIndexServlet.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class AdsIndexServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+
+@WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
+public class AdsIndexServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Ads adsDao = DaoFactory.getAdsDao();
+
+        List<Ad> currentAds = adsDao.all();
+        request.setAttribute("allAds", currentAds);
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+    }
 }
