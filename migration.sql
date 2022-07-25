4aed1611b229f3838ae3ccd9e56014e41a2ec656
Chris Davis <christopher.j.davis03@gmail.com>
2022-07-25 12:28:34 -0500

    established many to many relationship

--- migration.sql
+++ migration.sql
@@ -1,7 +1,11 @@
 USE adlister_db;
 
+SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS ads_categories;
+SET foreign_key_checks = 1;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -19,4 +23,17 @@ CREATE TABLE ads (
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
+);
+
+CREATE TABLE categories (
+    id INT  UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(240),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ads_categories (
+    ad_id INT UNSIGNED NOT NULL,
+    cat_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (ad_id) REFERENCES ads(id),
+    FOREIGN KEY (cat_id) REFERENCES categories(id)
 );
