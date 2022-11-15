5160bddc4149f3764d93a3d72f1d0497b3142d60
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-11-15 15:18:02 -0600

    delete method added, User can delete their add from profile

--- src/main/java/com/codeup/adlister/controllers/DeleteAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteAdServlet.java
@@ -11,14 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "DeleteAdServlet", urlPatterns = "ads/delete")
+@WebServlet(name = "DeleteAdServlet", urlPatterns = "/ads/delete")
 public class DeleteAdServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getSession().getAttribute("user") != null) {
             long adId = Long.parseLong(req.getParameter("id"));
             DaoFactory.getAdsDao().delete(adId);
-            req.getRequestDispatcher("WEB-INF/ads/adindex.jsp").forward(req, resp);
+            resp.sendRedirect("/profile");
+//            req.getRequestDispatcher("WEB-INF/profile.jsp").forward(req, resp);
         } else {
             resp.sendRedirect("/login");
         }
