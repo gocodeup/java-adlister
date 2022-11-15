a6b91206b9bf4489f897eb233bd7bdc812058e1f
Cody Palmer <codyryanpalmer@gmail.com>
2022-11-15 11:42:33 -0600

    Updates to search / nav bar

---
+++ src/main/java/com/codeup/adlister/controllers/SearchServlet.java
@@ -0,0 +1,17 @@
+package com.codeup.adlister.controllers;
+import com.codeup.adlister.dao.DaoFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SearchServlet", urlPatterns = "/ads")
+public class SearchServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.getRequestDispatcher("/WEB-INF/ads/searchAds.jsp").forward(request, response);
+    }
+}
--- src/main/webapp/WEB-INF/partials/navbarLoggedIn.jsp
+++ src/main/webapp/WEB-INF/partials/navbarLoggedIn.jsp
@@ -33,6 +33,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/ads/create">Create Ads</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/ads">Ads</a>
+                </li>
             </ul>
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
