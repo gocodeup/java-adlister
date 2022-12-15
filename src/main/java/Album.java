bc6b374169d695ed0cf84a269d2adf3e75f91f4e
Matthew Wiram <matthew.wiram551@gmail.com>
2022-12-15 15:02:42 -0500

    done

---
+++ src/main/java/Album.java
@@ -0,0 +1,61 @@
+import java.io.Serializable;
+
+public class Album implements Serializable {
+    private long id;
+    private String artist;
+    private String name;
+    private int release_date;
+    private float sales;
+    private String genre;
+    public Album() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(int release_date) {
+        this.release_date = release_date;
+    }
+
+    public float getSales() {
+        return sales;
+    }
+
+    public void setSales(float sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
